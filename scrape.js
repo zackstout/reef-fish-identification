e5179bd643465bbc414c579462dee54cfbc12e6b
Zack Stout <zackstout@gmail.com>
2024-09-04 14:31:12 -0500

    Commit

--- scrape.js
+++ scrape.js
@@ -45,204 +45,204 @@ const parseFishData = async (fishName) => {
 };
 
 const fishList = [
-  //   "Scrawled cowfish",
-  //   "French angelfish",
-  //   "Queen angelfish",
-  //   "Queen triggerfish",
-  //   "Banded butterflyfish",
-  //   "Foureye butterflyfish",
-
-  //   "Gray angelfish",
-  //   "Blue tang",
-  //   "Ocean surgeonfish",
-  //   "Queen parrotfish",
-  //   "Striped parrotfish",
-  //   "Stoplight parrotfish",
-  //   "Princess parrotfish",
-  //   "Tiger grouper",
-  //   "Coney",
-  //   "French grunt",
-  //   "Smallmouth grunt",
-  //   "Bluestriped grunt",
-  //   "Black margate",
-  //   "Schoolmaster snapper",
-  //   "Yellowtail snapper",
-  //   "Black durgon",
-  //   "Whitespotted filefish",
-  //   "Hogfish",
-  //   "Spanish hogfish",
-
-  //   "Green sea turtle",
-  //   "Hawksbill sea turtle",
-  //   "Bluehead",
-  //   "Yellowhead wrasse",
-  //   "Clown wrasse",
-  //   "Rainbow wrasse",
-  //   "Yellowcheek wrasse",
-  //   "Greenband wrasse",
-  //   "Painted wrasse",
-  //   "Midnight parrotfish",
-  //   "Blue parrotfish",
-  //   "Rainbow parrotfish",
-  //   "Redtail parrotfish",
-  //   "Yellowhead jawfish",
-  //   "Banded jawfish",
-  //   "Great barracuda",
-  //   "Palehead blenny",
-  //   "Blue gobi",
-  //   "Yellowtail damselfish",
-  //   "Surgeonfish",
-  //   "Blue chromis",
-  //   "Rock beauty",
-  //   "Blue angelfish",
-  //   "Reef butterflyfish",
-  //   "Threeband butterflyfish",
-  //   "Spotted drum",
-  //   "White grunt",
-  //   "Sheepshead",
-  //   "Mutton snapper",
-  //   "Yellowtail snapper",
-  //   "Bar Jack",
-  //   "Palometa",
-  //   "Bigeye scad",
-  //   "Glasseye snapper",
-  //   "Pale cardinalfish",
-  //   "Fairy basslet",
-  //   "Blackcap basslet",
-  //   "Mutton hamlet",
-  //   "Black grouper",
-  //   "Reef bass",
-  //   "Sand perch",
-  //   "Nassau grouper",
-  //   "Lined seahorse",
-  //   "Spotted scorpionfish",
-  //   "Reef scorpionfish",
-  //   "Lionfish",
-  //   "Reef squirrelfish",
-  //   "Coral toadfish",
-  //   "Dwarf frogfish",
-  //   "Tarpon",
-  //   "Green moray eel",
-  //   "Spotted moray eel",
-  //   "Purplemouth moral eel",
-  //   "Goldentail moral eey",
-  //   "Spotted eagle ray",
-  //   "Southern stingray",
-  //   "Yellow stingray",
-  //   "Lesser electric ray",
-  //   "Guitarfish",
-  //   "Manta ray",
-  //   "Nurse shark",
-  //   "Whitespotted puffer",
-  //   "Spotted trunkfish",
-  //   "Yellow boxfish",
-  //   "Whitespotted pygmy filefish",
-  //   "Broom filefish",
-  //   "Peacock flounder",
-  //   "Black durgon",
-
-  //   "Royal grammma", // Fairy basslet
-  //   "Cephalopholis fulva", // Coney
-  //   "Paracanthurus hepatus", // Blue tang
-  //   "Halichoeres maculipinna", // clown wrasse
-  //   "Archosargus probatocephalus", // sheepshead
-  //   "Haemulon chrysargyreum", // small mouth grunt
-  //   "Cantherhines macrocerus", // white spotted filefish
-  //   "Slippery dick",
-  //   "Three-banded butterflyfish",
-  //   "Purplemouth moray eel",
-  //   "Gymnothorax miliaris", // goldentail moray
-  //   "White-spotted puffer",
-  //   "Porcupinefish",
-  //   "Whitetip reef shark",
-  //   "Sergeant major (fish)",
-  //   "Stenorhynchus seticornis", // arrow crab
-  //   "Maguimithrax spinosissimus", // caribbean king crab
-  //   "Mithraculus cinctimanus", // some crab
-  //   "Panulirus argus", // caribbean spiny lobster
-  //   "Caribbean reef squid",
-  //   "Caribbean reef octopus",
-  //   "Comb jellyfish",
-
-  //   "Loggerhead sea turtle",
-  //   "Aluterus schoepfii", // orange filefish
-  //   "Monacanthus ciliatus", // fringed filefish
-  //   "Stegastes leucostictus", // beaugregory (damselfish)
-  //   "Stegastes partitus", // bicolor damselfish
-  //   "Butter hamlet",
-  //   "Shy hamlet",
-  //   "Indigo hamlet",
-  //   "Sunshinefish",
-  //   "Brown chromis",
-  //   "Longsnout butterflyfish",
-  //   "Spotfin butterflyfish",
-  //   "Red hind",
-  //   "Harlequin bass",
-  //   "Tobaccofish",
-  //   "Cave basslet",
-  //   "Acanthurus bahianus", // ocean surgeonfish
-  //   "Green razorfish",
-  //   "Pearly razorfish",
-  //   "Porkfish",
-  //   "Dog snapper",
-  //   "Cubera snapper",
-  //   "Mahogany snapper",
-  //   "Blackfin snapper",
-  //   "Green humphead parrotfish",
-  //   "Ocean triggerfish",
-  //   "Sargassum triggerfish",
-  //   "Smooth trunkfish",
-  //   "Honeycomb trunkfish",
-  //   "Scrawled filefish",
-  //   "Longspine squirrelfish",
-  //   "Reef squirrelfish",
-  //   "Yellowhead jawfish",
-  //   "Balloonfish",
-  //   "Jack-knifefish",
-  //   "Pareques acuminatus", // high hat fish, like drums
-  //   "Trumpetfish",
-  //   "Spotted goatfish",
-  //   "Yellow goatfish",
-  //   "Bermuda chub",
-  //   "Darkfin chub",
-  //   "Bearded toadfish",
-  //   "Red lionfish",
-  //   "Channel flounder",
-  //   "Eyed flounder",
-  //   "Maculated flounder",
-  //   "Viper moray eel",
-  //   "Brown garden eel",
-  //   "Caribbean blenny",
-  //   "Flamingo tongue snail",
-  //   "Sand diver",
-  //   "Red lizardfish",
-  //   "Sheepshead porgy",
-  //   "Jothead porgy",
-  //   "Caribbean whiptail stingray",
-
-  "Spotted spiny lobster",
-  "Queen conch",
-  "Bearded fireworm",
-  "Christmas tree worm",
-  "Magnificent feather duster",
-  "Brown fanworm",
-  "Moon jelly",
-  "Comb jelly",
-  "Thimble jelly",
-  "Holothuria mexicana", // donkey dung sea cucumber LOL
-  "Channel clinging crab",
-  "Nimble spray crab",
-  "Hairy clinging crab",
-  "Shortfinger neck crab",
-  "Neck crab",
-  "Reticulated brittle star",
-  "Giant hermit crab",
-  "Red reef hermit crab",
-  "Banded coral shrimp",
-  "Spotted cleaner shrimp",
-  "Dark mantis shrimp",
-  "Giant basket star",
-
+  // "Scrawled cowfish",
+  // "French angelfish",
+  // "Queen angelfish",
+  // "Queen triggerfish",
+  // "Banded butterflyfish",
+  // "Foureye butterflyfish",
+  // "Gray angelfish",
+  // "Blue tang",
+  // "Ocean surgeonfish",
+  // "Queen parrotfish",
+  // "Striped parrotfish",
+  // "Stoplight parrotfish",
+  // "Princess parrotfish",
+  // "Tiger grouper",
+  // "Coney",
+  // "French grunt",
+  // "Smallmouth grunt",
+  // "Bluestriped grunt",
+  // "Black margate",
+  // "Schoolmaster snapper",
+  // "Yellowtail snapper",
+  // "Black durgon",
+  // "Whitespotted filefish",
+  // "Hogfish",
+  // "Spanish hogfish",
+  // "Green sea turtle",
+  // "Hawksbill sea turtle",
+  // "Bluehead",
+  // "Yellowhead wrasse",
+  // "Clown wrasse",
+  // "Rainbow wrasse",
+  // "Yellowcheek wrasse",
+  // "Greenband wrasse",
+  // "Painted wrasse",
+  // "Midnight parrotfish",
+  // "Blue parrotfish",
+  // "Rainbow parrotfish",
+  // "Redtail parrotfish",
+  // "Yellowhead jawfish",
+  // "Banded jawfish",
+  // "Great barracuda",
+  // "Palehead blenny",
+  // "Blue gobi",
+  // "Yellowtail damselfish",
+  // "Surgeonfish",
+  // "Blue chromis",
+  // "Rock beauty",
+  // "Blue angelfish",
+  // "Reef butterflyfish",
+  // "Threeband butterflyfish",
+  // "Spotted drum",
+  // "White grunt",
+  // "Sheepshead",
+  // "Mutton snapper",
+  // "Yellowtail snapper",
+  // "Bar Jack",
+  // "Palometa",
+  // "Bigeye scad",
+  // "Glasseye snapper",
+  // "Pale cardinalfish",
+  // "Fairy basslet",
+  // "Blackcap basslet",
+  // "Mutton hamlet",
+  // "Black grouper",
+  // "Reef bass",
+  // "Sand perch",
+  // "Nassau grouper",
+  // "Lined seahorse",
+  // "Spotted scorpionfish",
+  // "Reef scorpionfish",
+  // "Lionfish",
+  // "Reef squirrelfish",
+  // "Coral toadfish",
+  // "Dwarf frogfish",
+  // "Tarpon",
+  // "Green moray eel",
+  // "Spotted moray eel",
+  // "Purplemouth moral eel",
+  // "Goldentail moral eey",
+  // "Spotted eagle ray",
+  // "Southern stingray",
+  // "Yellow stingray",
+  // "Lesser electric ray",
+  // "Guitarfish",
+  // "Manta ray",
+  // "Nurse shark",
+  // "Whitespotted puffer",
+  // "Spotted trunkfish",
+  // "Yellow boxfish",
+  // "Whitespotted pygmy filefish",
+  // "Broom filefish",
+  // "Peacock flounder",
+  // "Black durgon",
+  // "Royal grammma", // Fairy basslet
+  // "Cephalopholis fulva", // Coney
+  // "Paracanthurus hepatus", // Blue tang
+  // "Halichoeres maculipinna", // clown wrasse
+  // "Archosargus probatocephalus", // sheepshead
+  // "Haemulon chrysargyreum", // small mouth grunt
+  // "Cantherhines macrocerus", // white spotted filefish
+  // "Slippery dick",
+  // "Three-banded butterflyfish",
+  // "Purplemouth moray eel",
+  // "Gymnothorax miliaris", // goldentail moray
+  // "White-spotted puffer",
+  // "Porcupinefish",
+  // "Whitetip reef shark",
+  // "Sergeant major (fish)",
+  // "Stenorhynchus seticornis", // arrow crab
+  // "Maguimithrax spinosissimus", // caribbean king crab
+  // "Mithraculus cinctimanus", // some crab
+  // "Panulirus argus", // caribbean spiny lobster
+  // "Caribbean reef squid",
+  // "Caribbean reef octopus",
+  // "Comb jellyfish",
+  // "Loggerhead sea turtle",
+  // "Aluterus schoepfii", // orange filefish
+  // "Monacanthus ciliatus", // fringed filefish
+  // "Stegastes leucostictus", // beaugregory (damselfish)
+  // "Stegastes partitus", // bicolor damselfish
+  // "Butter hamlet",
+  // "Shy hamlet",
+  // "Indigo hamlet",
+  // "Sunshinefish",
+  // "Brown chromis",
+  // "Longsnout butterflyfish",
+  // "Spotfin butterflyfish",
+  // "Red hind",
+  // "Harlequin bass",
+  // "Tobaccofish",
+  // "Cave basslet",
+  // "Acanthurus bahianus", // ocean surgeonfish
+  // "Green razorfish",
+  // "Pearly razorfish",
+  // "Porkfish",
+  // "Dog snapper",
+  // "Cubera snapper",
+  // "Mahogany snapper",
+  // "Blackfin snapper",
+  // "Green humphead parrotfish",
+  // "Ocean triggerfish",
+  // "Sargassum triggerfish",
+  // "Smooth trunkfish",
+  // "Honeycomb trunkfish",
+  // "Scrawled filefish",
+  // "Longspine squirrelfish",
+  // "Reef squirrelfish",
+  // "Yellowhead jawfish",
+  // "Balloonfish",
+  // "Jack-knifefish",
+  // "Pareques acuminatus", // high hat fish, like drums
+  // "Trumpetfish",
+  // "Spotted goatfish",
+  // "Yellow goatfish",
+  // "Bermuda chub",
+  // "Darkfin chub",
+  // "Bearded toadfish",
+  // "Red lionfish",
+  // "Channel flounder",
+  // "Eyed flounder",
+  // "Maculated flounder",
+  // "Viper moray eel",
+  // "Brown garden eel",
+  // "Caribbean blenny",
+  // "Flamingo tongue snail",
+  // "Sand diver",
+  // "Red lizardfish",
+  // "Sheepshead porgy",
+  // "Jothead porgy",
+  // "Caribbean whiptail stingray",
+  // "Spotted spiny lobster",
+  // "Queen conch",
+  // // "Bearded fireworm",
+  // "Hermodice carunculata", // bearded fireworm with class data
+  // // "Christmas tree worm",
+  // "Spirobranchus giganteus", // christmas tree worm
+  // "Magnificent feather duster",
+  // "Brown fanworm",
+  // "Moon jelly",
+  // "Lampocteis", // blood belly comb jelly
+  // // "Comb jelly",
+  // "Thimble jelly",
+  // "Holothuria mexicana", // donkey dung sea cucumber LOL
+  // "Channel clinging crab",
+  // "Nimble spray crab",
+  // "Hairy clinging crab",
+  // "Shortfinger neck crab",
+  // "Neck crab",
+  // "Reticulated brittle star",
+  // "Giant hermit crab",
+  // "Red reef hermit crab",
+  // "Banded coral shrimp",
+  // "Spotted cleaner shrimp",
+  // "Dark mantis shrimp",
+  // "Giant basket star",
+  //
+  // ADD MORE MARINE ANIMALS HERE
+  //
   // NOTE: many wrasses lack individual wikis, https://en.wikipedia.org/wiki/Halichoeres (genus)
   // Same for gobys, https://en.wikipedia.org/wiki/Elacatinus (genus)
   // Ditto for parrots, https://en.wikipedia.org/wiki/Scarus (genus)
@@ -259,6 +259,13 @@ const run = async () => {
   const itv = setInterval(async () => {
     const fishName = fishList[idx];
 
+    if (fishName in result) {
+      total++;
+      idx++;
+      console.log("we have already SEEN", fishName);
+      return;
+    }
+
     const fishData = await parseFishData(fishName);
     result[fishName] = fishData;
     console.log(`Processed ${fishName}.`);
@@ -272,8 +279,10 @@ const run = async () => {
         result,
         `${total - errors} / ${total} successful!`
       );
+
+      pbcopy(JSON.stringify(result));
     }
-  }, 1000);
+  }, 200);
   //   const cowfish = await parseFishData("Scrawled cowfish");
 
   //   console.log(cowfish);
@@ -281,4 +290,20 @@ const run = async () => {
 
 run();
 
+// test();
+
+function test() {
+  pbcopy(JSON.stringify({ testing: "what up" }));
+}
+
 // Wow it actually works
+
+function pbcopy(data) {
+  var proc = require("child_process").spawn("pbcopy");
+  proc.stdin.write(data);
+  proc.stdin.end();
+}
+
+/**
+ * Omg, some don't have "class" but instead "clade" or "subclass".... I'm just manually amending the worms
+ */
